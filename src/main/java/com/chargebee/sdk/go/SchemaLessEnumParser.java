bad22a3d34359afcb671a19cdcca28373aec0fb5
cb-alish <alish.sapkota@chargebee.com>
2025-08-13 11:12:42 +0530

    Schemaless enums bug fixes for go

--- src/main/java/com/chargebee/sdk/go/SchemaLessEnumParser.java
+++ src/main/java/com/chargebee/sdk/go/SchemaLessEnumParser.java
@@ -21,58 +21,66 @@ public class SchemaLessEnumParser {
     for (Resource res : resourceList) {
       for (Action action : res.getSortedAction()) {
         for (Parameter iParam : action.requestBodyParameters()) {
+          List<Attribute> atter = new ArrayList<>();
+          Attribute attribute = new Attribute(iParam.getName(), iParam.schema, iParam.isRequired);
           if ((iParam.schema instanceof ObjectSchema || iParam.schema instanceof MapSchema)
               && iParam.schema.getProperties() != null
               && iParam.schema.getItems() == null
               && !iParam.isCompositeArrayBody()) {
-            Attribute attribute = new Attribute(iParam.getName(), iParam.schema, iParam.isRequired);
             if (attribute.isFilterAttribute()) continue;
-            attribute
-                .attributes()
-                .forEach(
-                    value -> {
-                      if (!(value.isEnumAttribute() || value.isGlobalEnumAttribute())) {
-                        return;
-                      }
-                      if (!value.isNotHiddenAttribute()) return;
-                      if (value.isGlobalEnumAttribute()
-                          || (value.isDeprecated() && value.isEnumAttribute())) {
-                        return;
-                      }
-                      boolean isResourceLevelEnum =
-                          res.attributes().stream().anyMatch(a -> a.name.equals(iParam.getName()));
-                      if (value.isEnumAttribute() && isResourceLevelEnum) return;
-                      if (value.metaModelName() != null) {
-                        String parentResourceName =
-                            Inflector.capitalize(getName(singularize(value.metaModelName())));
-                        List<Resource> filteredResources =
-                            resourceList.stream()
-                                .filter(
-                                    resource -> Objects.equals(resource.name, parentResourceName))
-                                .toList();
-                        if (filteredResources.isEmpty()) {
-                          return;
-                        }
-                        Resource parentResource = filteredResources.get(0);
-                        boolean isSchemaLessEnum =
-                            parentResource.attributes().stream()
-                                .noneMatch(
-                                    parentAttribute ->
-                                        Objects.equals(parentAttribute.name, value.name));
-                        if (isSchemaLessEnum
-                            && Objects.equals(activeResource.name, parentResourceName)) {
-                          Enum tempEnum = new Enum(value.name, value.schema);
-                          if (enumList.stream()
-                              .noneMatch(
-                                  _enum ->
-                                      _enum.name.equals(tempEnum.name)
-                                          && _enum.validValues().equals(tempEnum.validValues()))) {
-                            enumList.add(tempEnum);
-                          }
-                        }
-                      }
-                    });
+            atter.addAll(attribute.attributes());
           }
+          atter.add(attribute);
+          atter.forEach(
+              value -> {
+                if (!(value.isEnumAttribute() || value.isGlobalEnumAttribute())) {
+                  return;
+                }
+                if (!value.isNotHiddenAttribute()) return;
+                if (value.isGlobalEnumAttribute()
+                    || (value.isDeprecated() && value.isEnumAttribute())) {
+                  return;
+                }
+                boolean isResourceLevelEnum =
+                    res.attributes().stream().anyMatch(a -> a.name.equals(iParam.getName()));
+                if (value.isEnumAttribute() && isResourceLevelEnum) return;
+                if (value.metaModelName() != null) {
+                  String parentResourceName =
+                      Inflector.capitalize(getName(singularize(value.metaModelName())));
+                  List<Resource> filteredResources =
+                      resourceList.stream()
+                          .filter(resource -> Objects.equals(resource.name, parentResourceName))
+                          .toList();
+                  if (filteredResources.isEmpty()) {
+                    return;
+                  }
+                  Resource parentResource = filteredResources.get(0);
+                  boolean isSchemaLessEnum =
+                      parentResource.attributes().stream()
+                          .noneMatch(
+                              parentAttribute -> Objects.equals(parentAttribute.name, value.name));
+                  if (isSchemaLessEnum && Objects.equals(activeResource.name, parentResourceName)) {
+                    Enum tempEnum = new Enum(value.name, value.schema);
+                    if (enumList.stream()
+                        .noneMatch(
+                            _enum ->
+                                _enum.name.equals(tempEnum.name)
+                                    && _enum.validValues().equals(tempEnum.validValues()))) {
+                      enumList.add(tempEnum);
+                    }
+                  }
+                } else if (value.metaModelName() == null
+                    && Objects.equals(res.name, activeResource.name)) {
+                  Enum tempEnum = new Enum(value.name, value.schema);
+                  if (enumList.stream()
+                      .noneMatch(
+                          _enum ->
+                              _enum.name.equals(tempEnum.name)
+                                  && _enum.validValues().equals(tempEnum.validValues()))) {
+                    enumList.add(tempEnum);
+                  }
+                }
+              });
         }
       }
     }
