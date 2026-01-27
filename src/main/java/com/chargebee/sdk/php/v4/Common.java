123127e6c5c4b519044042ab5c60a26615af153f
cb-alish <alish.sapkota@chargebee.com>
2026-01-27 14:20:51 +0530

    fix(php): correct enum typing for sub-resource properties

--- src/main/java/com/chargebee/sdk/php/v4/Common.java
+++ src/main/java/com/chargebee/sdk/php/v4/Common.java
@@ -1,7 +1,6 @@
 package com.chargebee.sdk.php.v4;
 
-import static com.chargebee.GenUtil.singularize;
-import static com.chargebee.GenUtil.toCamelCase;
+import static com.chargebee.GenUtil.*;
 import static com.chargebee.openapi.Extension.SDK_ENUM_API_NAME;
 import static com.chargebee.openapi.Extension.SUB_RESOURCE_NAME;
 import static com.chargebee.openapi.Resource.*;
@@ -16,6 +15,7 @@ import io.swagger.v3.oas.models.media.*;
 public class Common {
 
   public static final String CHARGEBEE_ENUMS_BASE_PATH = "\\Chargebee\\Enums\\";
+
   public static final String CHARGEBEE_RESOURCES_BASE_PATH = "\\Chargebee\\Resources\\";
   public static final String ENUMS_DIRECTORY = "Enums";
 
@@ -159,6 +159,51 @@ public class Common {
         attribute, CHARGEBEE_ENUMS_BASE_PATH, String.format("%sType", singularize(type)));
   }
 
+  public static Column globalEnumsForSubResources(Attribute attribute) {
+    return createEnumColumn(attribute, CHARGEBEE_ENUMS_BASE_PATH);
+  }
+
+  public static Column localEnumsForSubResources(
+      Attribute attribute, Resource res, Attribute subResourceAttribute) {
+    String type;
+    String resourceName = res.name;
+
+    if (subResourceAttribute.isDependentAttribute() || attribute.isDependentAttribute()) {
+      type = toCamelCase(res.name) + toCamelCase(attribute.name);
+    } else {
+      if (attribute.isExternalEnum()) {
+        if (attribute.getEnumApiName() == null
+            || attribute.getEnumApiName().equalsIgnoreCase(attribute.name)) {
+          type = toCamelCase(attribute.name);
+        } else {
+          type = firstCharLower(attribute.getEnumApiName());
+          resourceName = attribute.getEnumApiName();
+          type =
+              type.contains(".")
+                  ? type.substring(type.lastIndexOf('.') + 1)
+                  : toCamelCase(attribute.name);
+          resourceName =
+              resourceName.contains(".")
+                  ? resourceName.substring(0, resourceName.lastIndexOf('.'))
+                  : "";
+        }
+      } else {
+        type = subResourceName(subResourceAttribute, attribute);
+      }
+    }
+    String basePath;
+    // When an enum is marked as external but has no external reference in the OpenAPI specs,
+    // it is treated as a global enum.
+    if (resourceName.equals("")) {
+      basePath = CHARGEBEE_ENUMS_BASE_PATH;
+    } else {
+      basePath =
+          CHARGEBEE_RESOURCES_BASE_PATH + resourceName + BACK_SLASH + ENUMS_DIRECTORY + BACK_SLASH;
+    }
+
+    return createEnumColumn(attribute, basePath, type);
+  }
+
   public static Column localEnumParser(Attribute attribute, Resource res) {
     return createEnumColumn(
         attribute,
@@ -179,12 +224,12 @@ public class Common {
     return column;
   }
 
-  private static Column createEnumColumn(
-      Attribute attribute, String basePath, String fieldTypePHP) {
+  private static Column createEnumColumn(Attribute attribute, String basePath, String type) {
+    String enumType = basePath + type;
     Column column = new Column();
     column.setName(attribute.name);
-    column.setFieldTypePHP(basePath + fieldTypePHP);
-    column.setPhpDocField(basePath + fieldTypePHP);
+    column.setFieldTypePHP(enumType);
+    column.setPhpDocField(enumType);
     column.setIsOptional(true);
     column.setApiName(attribute.name);
     return column;
@@ -195,5 +240,12 @@ public class Common {
         + subResourceName(schema)
         + BACK_SLASH
         + subResourceName(schema);
+  }
+
+  private static String subResourceName(Attribute subResourceAttribute, Attribute attribute) {
+    if (subResourceAttribute.subResourceName() != null) {
+      return subResourceAttribute.subResourceName() + toCamelCase(attribute.name);
+    }
+    return singularize(toCamelCase(subResourceAttribute.name)) + toCamelCase(attribute.name);
   }
 }
